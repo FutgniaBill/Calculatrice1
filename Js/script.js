27092fd92f9d0bfbcbd1a6ddd343f4aa4ca8cdcb
Bill Wilfried <billwilfriedfutgnia@outlook.fr>
2020-10-30 11:37:55 +0100

    Modifier

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+console.log("Hello world");
+function value(valeur){
+console.log(valeur);
+var button=document.querySelector(".button");
+button.addEventListener("click",()=>{console.log(this)
+})
+}
+
